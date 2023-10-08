b2060c9495d9a73828df0579b48412e5db380e4c
Busra Aydin <56473075+bbaydn@users.noreply.github.com>
2023-10-09 00:23:20 +0300

    ViewModel and button added. Hard coded sections will be changed

---
+++ app/src/main/java/com/ba/mappy/ViewModel.kt
@@ -0,0 +1,38 @@
+package com.ba.mappy
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ba.mappy.model.Station
+import com.ba.mappy.network.RetrofitClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class StationViewModel : ViewModel() {
+
+    private val _stationsLiveData = MutableLiveData<List<Station>?>()
+    val stationsLiveData: MutableLiveData<List<Station>?> = _stationsLiveData
+
+    init {
+        fetchStations()
+    }
+
+    private fun fetchStations() {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val apiService = RetrofitClient.create()
+                val response: Response<List<Station>> = apiService.getStations().execute()
+
+                if (response.isSuccessful) {
+                    val stations = response.body()
+                    _stationsLiveData.postValue(stations)
+                } else {
+                    // Hata durumu kodu eklenecek
+                }
+            } catch (e: Exception) {
+                // Hata durumu kodu eklenecek
+            }
+        }
+    }
+}
