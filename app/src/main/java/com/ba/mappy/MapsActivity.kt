41ca99db4161a0a6c7e088b494009cc4832b81f3
Busra Aydin <56473075+bbaydn@users.noreply.github.com>
2023-10-10 21:09:37 +0300

    Permission updated for "Only this time" option

--- app/src/main/java/com/ba/mappy/MapsActivity.kt
+++ app/src/main/java/com/ba/mappy/MapsActivity.kt
@@ -1,29 +1,30 @@
 package com.ba.mappy
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
+import com.ba.mappy.databinding.ActivityMapsBinding
+import com.ba.mappy.presentation.ListTripsActivity
+import com.ba.mappy.viewmodel.StationViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.ba.mappy.databinding.ActivityMapsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.gson.Gson
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -33,23 +34,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val stationViewModel: StationViewModel by viewModels()
     private val PERMISSION_REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonListTrips.setOnClickListener {
+            val intent = Intent(this, ListTripsActivity::class.java)
+            startActivity(intent)
+        }
+
         val permission = Manifest.permission.ACCESS_FINE_LOCATION
-        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(permission), PERMISSION_REQUEST_CODE)
-        } else {
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
             setupMap()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), PERMISSION_REQUEST_CODE)
         }
     }
 
     private fun setupMap() {
-        val width = 45 // Genişlik
-        val height = 45 // Yükseklik
+        val width = 45
+        val height = 45
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap)
         val paint = Paint()
@@ -58,7 +65,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val radius = Math.min(width, height) / 2.toFloat()
         canvas.drawCircle(width / 2.toFloat(), height / 2.toFloat(), radius, paint)
 
-// Özel işaretçi ikonunu oluşturun
         val markerIcon = BitmapDescriptorFactory.fromBitmap(bitmap)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -68,27 +74,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+            ) == PackageManager.PERMISSION_GRANTED
         ) {
-            return
-        }
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener(this, OnSuccessListener { location ->
-                if (location != null) {
-                    val userLatLng = LatLng(location.latitude, location.longitude)
-
-                  //  mMap.addMarker(MarkerOptions().position(userLatLng).title("My Location"))
-                  //  mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 15f))
-
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 15f), 2000, null)
-                    mMap.addMarker(MarkerOptions().position(userLatLng).title("My Location"))
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener(this) { location ->
+                    if (location != null) {
+                        val userLatLng = LatLng(location.latitude, location.longitude)
+                        mMap.animateCamera(
+                            CameraUpdateFactory.newLatLngZoom(userLatLng, 15f),
+                            2000,
+                            null
+                        )
+                        mMap.addMarker(MarkerOptions().position(userLatLng).title("My Location"))
+                    }
                 }
-            })
+        }
 
-        stationViewModel.stationsLiveData.observe(this, Observer { stations ->
+        stationViewModel.stationsLiveData.observe(this) { stations ->
             val gson = Gson()
             // Convert the list to a JSON string
             val json = gson.toJsonTree(stations).asJsonArray
@@ -104,14 +109,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     val location = LatLng(latitude, longitude)
                     val name = jsonarrays.get("name")
 
-                    mMap.addMarker(MarkerOptions()
-                        .position(location).title(name.toString())
-                        .icon(markerIcon))
+                    mMap.addMarker(
+                        MarkerOptions()
+                            .position(location).title(name.toString())
+                            .icon(markerIcon)
+                    )
                 }
             } else {
                 Log.e("BUSRA", "this is completely wrong")
             }
-        })
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
